cb8e6bed0e630ea0870b188faa1ea67464f7a75a
M8ng8l <chelovekula88@gmail.com>
2020-08-24 16:14:05 +0300

    fix

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -75,7 +75,7 @@ class MyApp extends App<any> {
       <>
         <Head>
           <title>
-            {publicRuntimeConfig.SITE_NAME} | Modern Open Source URL shortener.
+            {publicRuntimeConfig.SITE_NAME} | URL shortener.
           </title>
         </Head>
         <StoreProvider store={this.store}>
